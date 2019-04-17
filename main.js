8daa3c10e61d995d2545e38a5d9896db50079798
syn <vicdemilly@gmail.com>
2019-04-17 14:38:13 +0200

    Ajout h1/h2 + CSS + 2 lignes JS + commentaires

---
+++ main.js
@@ -0,0 +1,4 @@
+$('p:first').css('border', '5px green dashed');
+$('p:last').css('border', '5px green dashed');
+// les sélecteurs first et last considèrent les premiers et derniers éléments indiqués (ici 'p')
+// dashed stylise la bordure avec des pointillés
