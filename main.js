78eef095d7f8e0bbadfaeab405d0b1393f269176
syn <vicdemilly@gmail.com>
2019-04-23 12:30:55 +0200

    Simplification du code

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-$('p:first').css('border', '5px green dashed');
-$('p:last').css('border', '5px green dashed');
+$('p:first, p:last').css('border', '5px green dashed');
 // les sélecteurs first et last considèrent les premiers et derniers éléments indiqués (ici 'p')
 // dashed stylise la bordure avec des pointillés
